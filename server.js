3cb6663702f2c838ca75bfd85de5b4fa6f955430
Raed <raed_ra@yahoo.com>
2020-06-21 01:13:36 +0800

    heroku upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const router = require("./routes/router");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
